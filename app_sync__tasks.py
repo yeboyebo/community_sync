96f4e69a0497dbe56b03aa78501c11e3b35a4482
xavikortes <javiercortesveliz@gmail.com>
2020-07-21 13:16:42 +0200

    Arreglando el parche

---
+++ app_sync__tasks.py
@@ -0,0 +1,114 @@
+from YBUTILS import globalValues
+
+from controllers.base.default.managers.task_manager import TaskManager
+
+from controllers.base.magento.drivers.magento import MagentoDriver
+from controllers.base.store.drivers.psql_store import PsqlStoreDriver
+
+# from controllers.api.b2c.stocks.controllers.egstock_upload import EgStockUpload
+# from controllers.api.b2c.points.controllers.egpoints_upload import EgPointsUpload
+# from controllers.api.b2c.prices.controllers.egprices_upload import EgPricesUpload
+# from controllers.api.b2c.orders.controllers.egorders_download import EgOrdersDownload
+# from controllers.api.b2c.customers.controllers.egcustomers_download import EgCustomersDownload
+# from controllers.api.b2c.refounds.controllers.egrefounds_download import EgRefoundsDownload
+# from controllers.api.b2c.stock_inicial.controllers.egstock_inicial_recieve import EgStockInicialRecieve
+
+
+# from controllers.api.b2b.products.controllers.eg_products_upload import EgProductsUpload as b2bProducts
+# from controllers.api.b2b.orders.controllers.orders_download import OrdersDownload as b2bOrders
+# from controllers.api.b2b.tierprice.controllers.eg_tierprice_upload import EgB2bTierpriceUpload
+# from controllers.api.b2b.price.controllers.eg_price_upload import EgB2bPriceUpload
+# from controllers.api.b2b.inventory.controllers.eg_inventory_upload import EgB2bInventoryUpload
+# from controllers.api.b2b.customerrequest.controllers.eg_customerrequest_recieve import EgB2bCustomerrequestRecieve
+# from controllers.api.b2b.customers.controllers.eg_customers_upload import EgB2bCustomerUpload
+
+# from controllers.api.store.orders.controllers.egorders_download import EgStoreOrdersDownload
+
+# from controllers.api.mirakl.orders.controllers.eg_orders_download import EgMiraklOrdersDownload
+# from controllers.api.mirakl.shippingorders.controllers.eg_shipping_orders_download import EgMiraklShippingOrdersDownload
+# from controllers.api.mirakl.returns.controllers.eg_returns_download import EgMiraklReturnsDownload
+# from controllers.api.mirakl.offers.controllers.eg_offers_upload import EgMiraklOffersUpload
+# from controllers.api.mirakl.returnsvaldemoro.controllers.eg_returnsvaldemoro_download import EgMiraklReturnsValdemoroDownload
+# from controllers.api.mirakl.returnsvaldemoroid.controllers.eg_returnsvaldemoroid_download import EgMiraklReturnsValdemoroIdDownload
+# from controllers.api.mirakl.offersdate.controllers.eg_offersdate_upload import EgMiraklOffersDateUpload
+
+sync_object_dict = {
+    # "stock_upload": {
+    #     "sync_object": EgStockUpload,
+    #     "driver": MagentoDriver
+    # },
+    # "points_upload": {
+    #     "sync_object": EgPointsUpload,
+    #     "driver": MagentoDriver
+    # },
+    # "prices_upload": {
+    #     "sync_object": EgPricesUpload,
+    #     "driver": MagentoDriver
+    # },
+    # "orders_download": {
+    #     "sync_object": EgOrdersDownload,
+    #     "driver": MagentoDriver
+    # },
+    # "customers_download": {
+    #     "sync_object": EgCustomersDownload,
+    #     "driver": MagentoDriver
+    # },
+    # "store_orders_download": {
+    #     "sync_object": EgStoreOrdersDownload,
+    #     "driver": PsqlStoreDriver
+    # },
+    # "refounds_download": {
+    #     "sync_object": EgRefoundsDownload,
+    #     "driver": MagentoDriver
+    # },
+    # "stock_inicial_recieve": {
+    #     "sync_object": EgStockInicialRecieve
+    # },
+    # "products_b2b_upload": {
+    #     "sync_object": b2bProducts
+    # },
+    # "orders_b2b_download": {
+    #     "sync_object": b2bOrders
+    # },
+    # "b2b_tierprice_upload": {
+    #     "sync_object": EgB2bTierpriceUpload
+    # },
+    # "b2b_price_upload": {
+    #     "sync_object": EgB2bPriceUpload
+    # },
+    # "b2b_inventory_upload": {
+    #     "sync_object": EgB2bInventoryUpload
+    # },
+    # "b2b_customerrequest_recieve": {
+    #     "sync_object": EgB2bCustomerrequestRecieve
+    # },
+    # "b2b_customers_upload": {
+    #     "sync_object": EgB2bCustomerUpload
+    # },
+    # "mirakl_orders_download": {
+    #     "sync_object": EgMiraklOrdersDownload
+    # },
+    # "mirakl_shippingorders_download": {
+    #     "sync_object": EgMiraklShippingOrdersDownload
+    # },
+    # "mirakl_returns_download": {
+    #     "sync_object": EgMiraklReturnsDownload
+    # },
+    # "mirakl_offers_upload": {
+    #     "sync_object": EgMiraklOffersUpload
+    # },
+    # "mirakl_returnsvaldemoro_download": {
+    #     "sync_object": EgMiraklReturnsValdemoroDownload
+    # },
+    # "mirakl_returnsvaldemoroid_download": {
+    #     "sync_object": EgMiraklReturnsValdemoroIdDownload
+    # },
+    # "mirakl_offersdate_upload": {
+    #     "sync_object": EgMiraklOffersDateUpload
+    # }
+}
+
+task_manager = TaskManager(sync_object_dict)
+
+globalValues.registrarmodulos()
+cdDef = 10
