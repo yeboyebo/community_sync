be1d370d939a6d3bc7f8fe2dd644590d0ac7cba7
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2020-08-02 19:43:54 +0200

    Products

--- app_sync__tasks.py
+++ app_sync__tasks.py
@@ -7,6 +7,7 @@ from controllers.base.default.managers.task_manager import TaskManager
 
 # from controllers.api.community.orders.controllers.egorders_download import EgOrdersDownload
 from controllers.api.community.orders.controllers.cm_orders_post import CmB2COrdersPost
+from controllers.api.community.products.controllers.community_products_upload import communityProductsUpload as communityProducts
 
 sync_object_dict = {
     # "orders_download": {
@@ -16,6 +17,9 @@ sync_object_dict = {
     "b2c_orders_post": {
         "sync_object": CmB2COrdersPost
     },
+    "products_community_upload": {
+        "sync_object": communityProducts
+    }
 }
 
 task_manager = TaskManager(sync_object_dict)
