1eede541264bd8f93ce97663db0956fcce2fe952
xavikortes <javiercortesveliz@gmail.com>
2020-08-06 11:49:59 +0200

    Upload, process y logserializer changes

--- ctrl_api_community_orders__cm_log_serializer.py
+++ ctrl_api_community_orders__cm_log_serializer.py
@@ -15,8 +15,9 @@ class CmB2CLogSerializer(DefaultSerializer):
 
         self.set_string_value("fechaalta", start_date)
         self.set_string_value("horaalta", start_time)
-        self.set_string_value("cuerpolog", str(self.init_data).replace("'", "\""))
         self.set_string_value("codcomanda", codigo)
         self.set_string_value("website", "community")
+
+        self.data["cuerpolog"] = str(self.init_data).replace("'", "\"")
 
         return True
